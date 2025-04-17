ffdff723c2059ac87b2936a2ba9e498602a428ff
Adnan-edu <md.ashikadnan@gmail.com>
2025-04-18 02:38:11 +1000

    Detected tennis court keypoints-resnet50

--- Tennis-Analysis-System/main.py
+++ Tennis-Analysis-System/main.py
@@ -1,5 +1,6 @@
 from utils import read_video, save_video
 from trackers import PlayerTracker, BallTracker
+from court_line_detector import CourtLineDetector
 
 def main():
     # Read the input video
@@ -14,11 +15,18 @@ def main():
     ball_tracker = BallTracker(model_path="models/tennis_ball_best.pt")
     ball_detections = ball_tracker.detect_frames(video_frames, read_from_stub=True, stub_path="tracker_stubs/ball_detection.pkl") # Saving the output of this frame in pkl format. 
 
+    # Detect Tennis Court Keypoints
+    court_model_path = "models/keypoints_model_50.pth"
+    court_line_detector = CourtLineDetector(model_path=court_model_path)
+    court_keypoints = court_line_detector.predict(video_frames[0])
+
     # Draw output
     # Draw Player Bounding Boxes
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
     # Draw Tennis Ball Bounding Boxes
     output_video_frames = ball_tracker.draw_bboxes(output_video_frames, ball_detections)
+    # Draw Tennis Court Keypoints
+    output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
 
     # Save Video
     save_video(output_video_frames, "output_videos/output.avi")
